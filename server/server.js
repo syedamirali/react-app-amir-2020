55ef79fc54f8e85b25c68ba3c85198a182b786a6
Amir <alisyedamir2018@gmail.com>
2020-03-02 14:34:33 +0500

    Heroku Setup Test

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ const path=require('path');
 const app=express();
 
 const folderPath=path.join(__dirname,'../public');
+const port=process.env.PORT || 5000;
 app.use(express.static(folderPath));
 
-app.listen(5000,()=>{
+app.listen(port,()=>{
     console.log("I am Up");
 })
